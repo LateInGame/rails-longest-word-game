1947bd53ad8b584de9b654f8c50d1cb55b57e34a
David Deus <davidafonsodeus@outlook.com>
2022-11-14 15:48:13 +0000

    validation not done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,28 @@
+require "json"
+require "open-uri"
+
 class GamesController < ApplicationController
 
   def new
     @letters = [*'A'..'Z'].sample(10)
   end
 
+  def first_validation
+    words_array = params[:words].upcase.chars
+    if (words_array - @letters).empty?
+      # missing code
+    else
+      puts "Sorry but #{params[:word]} can't be built out of #{@letters}"
+    end
+  end
+
+  def english_validation
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    user_serialized = URI.open(url).read
+    user = JSON.parse('found')
+  end
+
   def score
-    raise
+
   end
 end
